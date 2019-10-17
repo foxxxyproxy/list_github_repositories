503cdaaa588758810dcfcfbde8e223f5742a55da
Alexandra Ignatova <alex.ignatova@gmail.com>
2019-10-17 11:23:08 +0200

    feat: add errors handler

--- helpers.js
+++ helpers.js
@@ -1,7 +1,23 @@
 export const startLoader = element => {
-    element.innerHTML = `<div class="loading-spinner"></div>`;
-}
+  element.innerHTML = `<div class="loading-spinner"></div>`;
+};
 
 export const stopLoader = (element, value) => {
-    element.textContent = value;
-}
+  element.textContent = value;
+};
+
+export const displayError = (element, value) => {
+  element.textContent = value;
+};
+
+export const enableFields = (...fields) => {
+  fields.forEach(field => {
+    field.disabled = false;
+  });
+};
+
+export const disableFields = (...fields) => {
+  fields.forEach(field => {
+    field.disabled = true;
+  });
+};
--- index.js
+++ index.js
@@ -1,33 +1,54 @@
 import { Backend } from "./backend.js";
-import { startLoader, stopLoader } from "./helpers.js";
+import {
+  startLoader,
+  stopLoader,
+  displayError,
+  disableFields,
+  enableFields
+} from "./helpers.js";
 
-const github_username = document.querySelector("#github-username");
+const github_username_form = document.querySelector("#github-username");
 const list = document.querySelector("#list");
 const button = document.querySelector("#get-repos");
+const form = document.querySelector("#repos-form");
+const login_error = document.querySelector("#login-error");
+
 const API = new Backend();
 API.setBaseUrl("https://api.github.com/");
 
-const form = document.querySelector("#repos-form");
 form.addEventListener("submit", event => {
   event.preventDefault();
+  disableFields(github_username_form);
+  displayError(login_error, "");
   startLoader(button);
-  if (!github_username.value) {
+
+  const user = github_username_form.value;
+
+  if (!user) {
+    displayError(login_error, "Input user name");
+    stopLoader(button, "Get repositories");
+    enableFields(github_username_form);
     return false;
   }
-  console.log(github_username.value);
-  const user = github_username.value;
 
   API.get(`users/${user}/repos`)
     .then(data => {
+      if (data.message) {
+        displayError(login_error, data.message);
+        return false;
+      }
+
       let html = "";
       data.forEach(item => {
         html +=
           ("beforeend",
-          `<li id="repo-item"><a href = "${item.html_url}" target = "_blank"><h2>${item.full_name}</h2> <p>${item.description}</p></a></li>`);
+          `<li id="repo-item"><a href = "${item.html_url}" target = "_blank">
+          <h2>${item.full_name}</h2> <p>${item.description}</p></a></li>`);
       });
       list.innerHTML = html;
     })
     .finally(() => {
       stopLoader(button, "Get repositories");
+      enableFields(github_username_form);
     });
 });
